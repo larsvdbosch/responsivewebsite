46baa1559b3ea5f16a7e4bb1f66cfae6373b0466
larsvdbosch <114734099+larsvdbosch@users.noreply.github.com>
2023-10-19 16:06:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+
+  // JavaScript to toggle the mobile navigation
+  document.getElementById("menu-icon").addEventListener("click", function () {
+    var mobileNav = document.getElementById("mobile-nav");
+    mobileNav.style.display = mobileNav.style.display === "block" ? "none" : "block";
+  });
